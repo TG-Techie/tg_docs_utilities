629f8b3171b9532250c73678633529d9936bf0d7
TG-Techie <TGTechie01@gmail.com>
2019-03-16 21:22:51 -0400

    updates

--- python_to_html.py
+++ python_to_html.py
@@ -1,5 +1,6 @@
 """
-can you see me?
+A utility for extracting documentation from ptyhon files
+as html websites.
 """
 import os, time, shutil, inspect
 
@@ -15,21 +16,21 @@ body{background-color: white;}
 #wrapper for a function
 '''<hr><div>
     <h3>{{name}}</h3>
-    <p>{{content}}</p>
+    <p>{{contents}}</p>
 </div>
 ''',
 #wrapper for a class
 '''
 <hr><div>
     <h3>{{name}}</h3>
-    <p>{{content}}</p>
+    <p>{{contents}}</p>
 </div>
 ''',
 #wrapper for a method of a class
 '''
 <hr><div>
     <h3>{{name}}</h3>
-    <p>{{content}}</p>
+    <p>{{contents}}</p>
 </div>
 ''',
 #start of the file
@@ -51,11 +52,12 @@ default_template.append(default_template[1])
 
 def parse_docstring(object, object_name, template):
     """
-    A function to parse the docs of a function, class, or method and return
+    Parse the docs of a function, class, or method and return
     a html formatted string.
     :param object: the object to extract docs from.
     :param object_name:the desired name used in the output.
-    :param template:the string to insert teh data into
+    :param template:the pre-written html to be formatted with the
+    object specific docs.
     """
     section = template
 
@@ -81,9 +83,16 @@ def parse_docstring(object, object_name, template):
             doc_out += '</li>\n'
         doc_out += '</ul>'
 
-    return(section.replace('{{content}}',doc_out))
+    return(section.replace('{{contents}}',doc_out))
 
 def write_function(func, func_name, file, template, index = 1):
+    """
+    From the template write the formatted docstring of the inputted function.
+    :param func: the function to document
+    :param func_name: the name to be used for the function.
+    :param file: the file to output the documentation to.
+    :param template: the list containing templates.
+    """
     string = parse_docstring(func, func_name, template[index])
     #print()
     #print(func_name, '"'+string+'"')
@@ -91,7 +100,13 @@ def write_function(func, func_name, file, template, index = 1):
         file.write(string+'\n')
 
 def write_class(class_obj, class_name, file, template):
-
+    """
+    From the template write the formatted docstring of the inputted class.
+    :param class_obj: the class to document.
+    :param class_name: the name to use when documenting.
+    :param file: the file to write the docs into.
+    :param template: the list containing templates.
+    """
     attr_list = list(dir(class_obj))
     attr_list.sort()
 
@@ -113,6 +128,17 @@ def write_class(class_obj, class_name, file, template):
     file.write('</ul>')
 
 def compile_page(module, output_path, template = default_template, layout = None):
+    """
+    Write a html document for the inputted module to the inputted location.
+    :param module: the module object to document.
+    :param output_path: the path to create the html file, locational input permitted.
+    :param template: a keyword input that specifies the wrapping html around documentation.
+    input a list in the order: [style, function, class, class_function, file_start, file_end].
+    style will be inserted into file_start in place of "{{style}}".
+    for function, class, and method  "{{name}}" and "{{contents}}" will be replaced with the name and content.
+    file_end and file_start will take take their places respectively on each side of the file.
+
+    """
     print()
     prev_path = os.path.abspath(output_path)
     file_path = os.path.abspath(output_path+'/docs/'+'/'.join(module.__name__.split('.')[0:-1]))
@@ -143,14 +169,14 @@ layout: {{layout}}
 
     #intro section
     file.write('<hr><ul>')
-    file.write('<h2 style="display:inline;">'+module_name+'</h2> ')
+    file.write('<h2>'+module_name+'</h2> ')
     file.write(module.__name__)
     if hasattr(module, '__version__'):
         if module.__version__:
             file.write('<p><h3 style="display:inline;">Version:</h3> '+module.__version__+"</p>")
     if hasattr(module, '__repo__'):
         if module.__repo__:
-            file.write('<p><h3 style="display:inline;">Repository:</h3> '+module.__repo__+"</p>")
+            file.write('<p><h3 style="display:inline;">Repository:</h3> <a href="'+module.__repo__+'">'+module.__repo__+'</a></p>')
     if hasattr(module, '__doc__'):
         if module.__doc__:
             file.write('<p><h3>Description:</h3> '+module.__doc__+"</p>")
